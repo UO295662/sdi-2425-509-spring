2d932627688a905e60de91bccedfd4da5e5f011e
UO295662 <UO295662@uniovi.es>
2025-02-02 11:08:02 +0100

    SDI-2425-509-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2425509spring/Sdi2425509SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2425509spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2425509SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2425509SpringApplication.class, args);
+    }
+
+}
