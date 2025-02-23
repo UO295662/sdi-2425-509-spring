4935eb54eb6f634948be7e027463be0a1d22d1db
UO295662 <UO295662@uniovi.es>
2025-02-23 12:38:31 +0100

    SDI-2425-509-5.4-Casos Uso VistaHome.

--- src/test/java/com/uniovi/sdi2425509spring/Sdi2425509SpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi2425509spring/Sdi2425509SpringApplicationTests.java
@@ -1,9 +1,14 @@
 package com.uniovi.sdi2425509spring;
 
+import com.uniovi.sdi2425509spring.pageobjects.PO_HomeView;
+import com.uniovi.sdi2425509spring.pageobjects.PO_Properties;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static java.sql.DriverManager.getDriver;
 
@@ -58,17 +63,32 @@ class Sdi2425509SpringApplicationTests {
 
 	@Test
 	@Order(1)
-	void PR01(){}
+	void PR01A(){
+		PO_HomeView.checkWelcomeToPage(driver, PO_Properties.getSPANISH());
+	}
 	@Test
 	@Order(2)
-	void PR02(){}
+	void PR01B(){
+		List<WebElement> welcomeMessageElement = PO_HomeView.getWelcomeMessageText(driver,
+				PO_Properties.getSPANISH());
+		Assertions.assertEquals(welcomeMessageElement.get(0).getText(),
+				PO_HomeView.getP().getString("welcome.message", PO_Properties.getSPANISH()));
+	}
 	@Test
 	@Order(3)
-	void PR03(){}@Test
+	void PR02(){
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}@Test
 	@Order(4)
-	void PR04(){}@Test
+	void PR03(){
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}@Test
 	@Order(5)
-	void PR05(){}
+	void PR04(){
+		PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish",
+				PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+
+	}
 	@Test
 	@Order(6)
 	void PR06(){}
