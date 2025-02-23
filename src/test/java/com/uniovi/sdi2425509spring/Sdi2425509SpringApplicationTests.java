e63a43174a1f535bde0d9b0fbeace58e42699d23
UO295662 <UO295662@uniovi.es>
2025-02-23 12:41:26 +0100

    SDI-2425-509-5.5-Casos Uso VistaSignUp.

--- src/test/java/com/uniovi/sdi2425509spring/Sdi2425509SpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi2425509spring/Sdi2425509SpringApplicationTests.java
@@ -2,6 +2,8 @@ package com.uniovi.sdi2425509spring;
 
 import com.uniovi.sdi2425509spring.pageobjects.PO_HomeView;
 import com.uniovi.sdi2425509spring.pageobjects.PO_Properties;
+import com.uniovi.sdi2425509spring.pageobjects.PO_SignUpView;
+import com.uniovi.sdi2425509spring.pageobjects.PO_View;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -74,30 +76,67 @@ class Sdi2425509SpringApplicationTests {
 		Assertions.assertEquals(welcomeMessageElement.get(0).getText(),
 				PO_HomeView.getP().getString("welcome.message", PO_Properties.getSPANISH()));
 	}
+	//PR02. Opción de navegación. Pinchar en el enlace Registro en la página home
 	@Test
 	@Order(3)
 	void PR02(){
 		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
-	}@Test
+	}
+	//PR03. Opción de navegación. Pinchar en el enlace Identifícate en la página home
+	@Test
 	@Order(4)
 	void PR03(){
 		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
-	}@Test
+	}
+	//PR04. Opción de navegación. Cambio de idioma de Español a Inglés y vuelta a Español
+	@Test
 	@Order(5)
 	void PR04(){
 		PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish",
 				PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
 
 	}
+	//PR05. Prueba del formulario de registro. registro con datos correctos
 	@Test
 	@Order(6)
-	void PR06(){}
+	void PR05(){
+		//Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		//Rellenamos el formulario.
+		PO_SignUpView.fillForm(driver, "77777778A", "Josefo", "Perez", "77777", "77777");
+		//Comprobamos que entramos en la sección privada y nos nuestra el texto a buscar
+		String checkText = "Notas del usuario";
+		List<WebElement> result = PO_View.checkElementBy(driver, "text", checkText);
+		Assertions.assertEquals(checkText, result.get(0).getText());
+	}
+	//PR06A. Prueba del formulario de registro. DNI repetido en la BD
+// Propiedad: Error.signup.dni.duplicate
 	@Test
 	@Order(7)
-	void PR07(){}
+	public void PR06A() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		PO_SignUpView.fillForm(driver, "99999990A", "Josefo", "Perez", "77777", "77777");
+		List<WebElement> result = PO_SignUpView.checkElementByKey(driver, "Error.signup.dni.duplicate",
+				PO_Properties.getSPANISH() );
+		//Comprobamos el error de DNI repetido.
+		String checkText = PO_HomeView.getP().getString("Error.signup.dni.duplicate",
+				PO_Properties.getSPANISH());
+		Assertions.assertEquals(checkText , result.get(0).getText());
+	}
+	//PR06B. Prueba del formulario de registro. Nombre corto.
+// Propiedad: Error.signup.dni.length
 	@Test
 	@Order(8)
-	void PR08(){}
+	public void PR06B() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		PO_SignUpView.fillForm(driver, "99999990B", "Jose", "Perez", "77777", "77777");
+		List<WebElement> result = PO_SignUpView.checkElementByKey(driver, "Error.signup.name.length",
+				PO_Properties.getSPANISH() );
+		//Comprobamos el error de Nombre corto de nombre corto .
+		String checkText = PO_HomeView.getP().getString("Error.signup.name.length",
+				PO_Properties.getSPANISH());
+		Assertions.assertEquals(checkText , result.get(0).getText());
+	}
 	@Test
 	@Order(9)
 	void PR09(){}
